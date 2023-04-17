0212bd8589f58e8778d832e3522e7741919ef1b4
Rushaan Gandhi <rushaansps@gmail.com>
2023-04-17 12:51:48 +0530

    --

--- ui/src/components/posts/Posts.js
+++ ui/src/components/posts/Posts.js
@@ -37,7 +37,7 @@ const Posts = () => {
     <Box>
       <Typography variant="h4" align="center">
         {" "}
-        Latest Recipes{" "}
+        Famous Recipes{" "}
       </Typography>
       <Grid
         container
